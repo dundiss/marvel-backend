1f967ed867f866d149bb38f54b726a7affa6240f
Diakary TRAORE <diakarytraore@MacBook-Pro-de-Diakary.local>
2022-02-27 01:42:11 +0100

    Gestion des filtres de la route characters

--- routes/characters.js
+++ routes/characters.js
@@ -9,9 +9,21 @@ const isAuthenticated = require("../middlewares/isAuthenticated");
 
 router.get('/characters', /*isAuthenticated,*/ async (req, res) => {
     try {
-        const response = await axios.get(
-            `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}`
-        );
+        let url = `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}`;
+        const { name, skip, limit } = req.query;
+        if (name) {
+            url += url+`&name=${name}`;
+        }
+
+        if (skip) {
+            url += url + `&skip=${skip}`;
+        }
+        
+        if (limit) {
+            url += url + `&limit=${limit}`;
+        }
+
+        const response = await axios.get( url );
         //console.log(response.data);
         res.json(response.data);
     } catch (error) {
